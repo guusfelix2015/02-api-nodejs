028c2b4addefa524cafad0756bffa2d8b716dcf0
guusfelix2015 <gusfelix2015@gmail.com>
2024-12-18 18:35:28 -0300

    listagem de transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -23,4 +23,23 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
     return reply.status(201).send()
   })
+
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const params = getTransactionParamsSchema.parse(request.params)
+
+    const { id } = params
+    const transactions = await knex('transactions').where('id', id).first()
+
+    return { transactions }
+  })
 }
