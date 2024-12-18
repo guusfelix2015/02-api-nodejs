81a0aa7350465b4f066d551998eddd5211ab8871
guusfelix2015 <gusfelix2015@gmail.com>
2024-12-18 15:18:17 -0300

    criação de transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,16 +1,26 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { z } from 'zod'
+import crypto from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transactions = knex('transactions')
-      .insert({
-        id: crypto.randomUUID(),
-        title: 'Transação de teste',
-        amount: 10000,
-      })
-      .returning('*')
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transactions
+    const body = createTransactionBodySchema.parse(request.body)
+
+    const { title, amount, type } = body
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
