00dd0fc0c349a510862dd81c6dde696e6caa75b9
guusfelix2015 <gusfelix2015@gmail.com>
2024-12-18 18:39:39 -0300

    resumo de transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -42,4 +42,12 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
     return { transactions }
   })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'ammount' })
+      .first()
+
+    return { summary }
+  })
 }
